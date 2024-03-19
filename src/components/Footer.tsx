6495935b156d52f1283bcae3a12b57135281afca
Yasseur Boutobba <y_boutobba@estin.dz>
2024-03-19 21:25:15 +0100

    initializing the project structre

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Footer</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,19 @@
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;

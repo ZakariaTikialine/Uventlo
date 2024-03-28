63fd01f5311d02ac9e4d3709e9a9fb67ed858941
zakaria <zakariatikia@gmail.com>
2024-03-28 17:54:28 +0100

    layout update

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,17 +1,22 @@
+'use client';
 import UserNavBar from "@/components/UserNavBar";
 import Sidebar from "@/components/SideBar";
 import SidebarEvent from "@/components/SideBarEvent";
+import { usePathname } from "next/navigation";
 
 export default function profileLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+  const isActive = ['/profile', '/profile/userDashboard', '/profile/userEvents', '/profile/userMessages', '/profile/userProfile'].includes(pathname);
   return (
     <main>
       <UserNavBar />
       <div className="flex">
-        <SidebarEvent />
+        <div className={`${isActive ? 'display' : 'hidden'}`}><Sidebar /></div>
+        <div className={`${isActive ? 'hidden' : 'display'}`}><SidebarEvent /></div>
         <div className="flex-1">{children}</div>
       </div>
     </main>
